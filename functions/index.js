bd460ed14439bc92115698a2082450d4068f1b97
Alexander Zhidkov <azhidkov@gmail.com>
2018-11-11 13:01:53 +0800

    add fcmSend function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.fcmSend = functions.firestore
+    .document('messages/{messageId}')
+    .onCreate((snap, context) => {
+        const message = snap.data();
+        const payload = {
+            notification: {
+                title: message.title,
+                body: message.body
+            }
+        };
+
+        return admin.messaging().sendToDevice(message.fcmToken, payload)
+    });
